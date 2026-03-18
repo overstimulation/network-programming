89408ca22dd4d801e34604be7ee294b404152dcd
overstimulation <146885536+overstimulation@users.noreply.github.com>
2026-03-18 23:22:49 +0100

    feat(lab-01): add solution for ex-04

---
+++ labs/lab-01/lab-01-ex-04.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+import sys
+import socket
+
+
+def main():
+    if len(sys.argv) != 2:
+        print(f"[ERROR] Usage: {sys.argv[0]} <ip_address>")
+        sys.exit(1)
+
+    ip_address = sys.argv[1]
+
+    try:
+        host_info = socket.gethostbyaddr(ip_address)
+        hostname = host_info[0]
+        print(f"[INFO] The hostname for {ip_address} is {hostname}")
+
+    except Exception as error:
+        print(f"[ERROR] Could not resolve hostname: {error}")
+
+
+if __name__ == "__main__":
+    main()
